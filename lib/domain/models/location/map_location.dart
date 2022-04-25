cdd449044b6a20a113cbc912cbb3a790ee6e5705
Sergey Kazantsev <serzh.kazantseff@gmail.com>
2022-04-25 20:13:44 +0500

    Add firebase and google maps dependencies

--- lib/domain/models/location/map_location.dart
+++ lib/domain/models/location/map_location.dart
@@ -1,8 +1,14 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class MapLocation {
-    final double latitude;
-    final double longitude;
+  final double latitude;
+  final double longitude;
 
-    MapLocation(this.latitude, this.longitude);
+  MapLocation(this.latitude, this.longitude);
 
-// todo create to and from google LatLng methods
+  MapLocation.fromGoogleLatLng(LatLng latLng)
+      : latitude = latLng.latitude,
+        longitude = latLng.longitude;
+
+  LatLng toGoogleLatLng() => LatLng(latitude, longitude);
 }
