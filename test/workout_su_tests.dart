926da1be73dbc0df7dc66f34684d6784f729778f
Sergey Kazantsev <serzh.kazantseff@gmail.com>
2022-06-07 17:37:27 +0500

    Test location of a single place in workout_su_tests.dart

--- test/workout_su_tests.dart
+++ test/workout_su_tests.dart
@@ -15,10 +15,12 @@ void main() {
       // https://workout.su/areas/9659
       var fullPlaceInfo = await parser.getFullInfoById(9659);
       expect(fullPlaceInfo.id, 9659);
-      expect(fullPlaceInfo.cityName, "Екатеринбург");
-      expect(fullPlaceInfo.locationName, "Проспект Ленина 85");
       expect(fullPlaceInfo.size, WorkoutSize.big);
       expect(fullPlaceInfo.novelty, WorkoutNovelty.modern);
+      expect(fullPlaceInfo.cityName, "Екатеринбург");
+      expect(fullPlaceInfo.locationName, "Проспект Ленина 85");
+      expect(fullPlaceInfo.location.latitude, 56.843124);
+      expect(fullPlaceInfo.location.longitude, 60.633194);
       expect(fullPlaceInfo.images.length, 12);
     });
   });
